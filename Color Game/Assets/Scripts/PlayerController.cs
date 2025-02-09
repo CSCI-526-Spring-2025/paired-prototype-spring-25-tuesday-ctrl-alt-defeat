65d6258c86645786c59b91ae74ed47ff8746c487
sriram1999s <>
2025-02-08 18:44:56 -0800

    added player and basic layout; added basic player movement

---
+++ Color Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Public Variables
+
+    // Speed of movement
+    public float speed = 10f;
+    // Determines how much player can jump
+    public float jumpForce = 10f;
+
+    // Assigned to groundcheck object
+    public Transform groundCheck;
+    // Radius used to check if player is grounded
+    public float checkRadius = 0.2f;
+    // Assigned to the ground
+    public LayerMask groundLayer;
+
+    // Private Variables
+
+    // Stores current rigid body
+    private Rigidbody2D rb;
+    // Stores horizontal movement input
+    private float moveInput;
+    // Flag to check if player is grounded
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Gets movement input
+        moveInput = Input.GetAxis("Horizontal"); // Get input (-1, 0, 1)
+
+        // Makes player jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Moves player
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+    }
+}
