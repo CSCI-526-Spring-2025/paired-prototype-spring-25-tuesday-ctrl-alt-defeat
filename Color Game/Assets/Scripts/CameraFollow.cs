821a777ca6b57142f4780732f1e00269f640b8b1
Soliloquy-0 <78271154+Soliloquy-0@users.noreply.github.com>
2025-02-11 23:17:48 -0800

    Add Camera follow
    add camera follow

---
+++ Color Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 6, -10);
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+
+            transform.position = target.position + offset;
+        }
+}
+}
