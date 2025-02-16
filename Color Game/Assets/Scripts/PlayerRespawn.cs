c965c4a9402f45669d0055d80b6e1fdb24cf616a
Soliloquy-0 <78271154+Soliloquy-0@users.noreply.github.com>
2025-02-15 17:10:39 -0800

    Respawn when falling or painted white
    Add player respawn

---
+++ Color Game/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    // sprite renderer
+    private SpriteRenderer spriteRenderer;
+    // record start position for respawn
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       startPosition = transform.position+ new Vector3(0, 2f, 0);
+       spriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if paint player in white then restart
+        Color currentColor = spriteRenderer.color;
+        if (IsColorWhite(currentColor))
+        {
+            Respawn(); 
+        }
+        // if fall, restart
+        if (transform.position.y < -17f) { 
+            Respawn();
+        }
+    }
+
+    
+    bool IsColorWhite(Color color)
+    {
+        return color.r > 0.95f && color.g > 0.95f && color.b > 0.95f;
+    }
+
+    void Respawn()
+    {
+        transform.position = startPosition;
+        spriteRenderer.color = Color.gray;
+    }
+}
