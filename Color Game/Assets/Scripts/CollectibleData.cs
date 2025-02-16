4c1de7707c2033ebd36cf33f13da36a958616d04
sriram1999s <>
2025-02-16 14:02:10 -0800

    added win condition mechanics with collectible counter

---
+++ Color Game/Assets/Scripts/CollectibleData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CollectibleData
+{
+    readonly public static int totalCollectibles = 4;
+    // current number of collectibles collected
+    public static int currCollectibles = 0;
+}
---
+++ Color Game/Assets/Scripts/PlayerWin.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using TMPro; // Import for TextMeshPro
+
+public class PlayerWin : MonoBehaviour
+{
+    public TMP_Text winMessageText; // Drag UI TextMeshPro object here
+    public GameObject playerObj;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Win condition Met");
+        WinGame();
+    }
+
+    void WinGame()
+    {
+        Debug.Log("You Win!");
+
+        // Get the player's Rigidbody2D component
+        Rigidbody2D rb = playerObj.GetComponent<Rigidbody2D>();
+
+        // Stop the player by freezing their velocity
+        rb.velocity = Vector2.zero;  // Stop movement
+
+        // Optionally, freeze the Rigidbody2D entirely (stop all motion)
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        
+        // Update the UI message with the static variable
+        winMessageText.text = "You Win! You have collected : " + CollectibleData.currCollectibles + " / " + CollectibleData.totalCollectibles + " collectibles!!";
+        winMessageText.color = Color.green;
+
+        winMessageText.gameObject.SetActive(true); // Show UI Message
+    }
+}
